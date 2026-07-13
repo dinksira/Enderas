f49f85062eb71e41441a5f2f233d10a95c0964ff
dinksira <dinksiraelisa@gmail.com>
2026-07-13 14:48:27 +0300

    Remove lock icon from tracking auth page

--- frontend/src/modules/tracking/views/TrackingAuthPage.jsx
+++ frontend/src/modules/tracking/views/TrackingAuthPage.jsx
@@ -34,13 +34,6 @@ function TrackingAuthPage() {
       </div>
 
       <div className="ta__card">
-        <div className="ta__icon-ring">
-          <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-            <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
-            <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-          </svg>
-        </div>
-
         <div className="ta__logo-wrap">
           <img src={logoUrl} alt="Enderas" className="ta__logo" />
         </div>
